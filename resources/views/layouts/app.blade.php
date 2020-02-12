69cbee9decbf4f04a4c79c117b488cfcad96cf6f
khaled-xiv <a.hachemi@esi-sba.dz>
2020-02-12 15:46:03 +0100

    styling the website

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,79 +2,79 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!-- Title -->
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Favicon -->
+    <link rel="shortcut icon" src="{{ asset ('favicon.ico')}}">
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Fontawesome -->
+    <link href="{{asset('css/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+
+    <!-- Bootstrap CSS -->
+    <link href="{{ asset('css/bootstrap/bootstrap.css') }}" rel="stylesheet" >
+
+    <!-- Animate Css -->
+    <link href="{{asset('css/animate/animate.min.css')}}" rel="stylesheet">
+
+    <!-- Custom Styles -->
+    <link href="{{asset('css/style.css')}}" rel="stylesheet">
+
+    <!-- Responsive  Css -->
+    <link href="{{asset('css/responsive.css')}}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    <!-- Preloader -->
+    <div id="preloader">
+        <div id="status">&nbsp;</div>
+    </div>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+    @yield('content')
 
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+    <!-- Footer  -->
+    <footer class="text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p>
+                        Copyright &copy; 2020 All Rights Reserved By <span>Naftal Inc.</span>
+                    </p>
                 </div>
             </div>
-        </nav>
+        </div>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+        <!-- Back To Top -->
+        <a href="#home" id="back-to-top" class="btn btn-sm btn-yellow btn-back-to-top smooth-scroll hidden-sm hidden-xs" title="home" role="button">
+            <i class="fa fa-angle-up"></i>
+        </a>
+
+    </footer>
+    <!-- Footer Ends -->
+
 </body>
+    <!-- Jquery -->
+    <script src="{{ asset('js/jquery.min.js') }}" defer></script>
+
+    <!--    Bootstrap-->
+    <script src="{{ asset('js/bootstrap/bootstrap.js') }}" defer></script>
+
+    <!--    Google MAP  -->
+    <script async defer
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB5thfF3yalaB_eqFj6SVd488_jmQMLkyI&callback=initMap">
+    </script>
+
+    <!--    Custom Script-->
+    <script src="{{ asset('js/script.js') }}" defer></script>
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,175 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
+@section('title', 'Home')
+@section('content')
+<!-- Home -->
+<section id="home">
 
-        <title>Laravel</title>
+    <!-- Background Video -->
+    <video id="home-bg-video" poster="video/solo.jpg" autoplay loop muted>
+        <source src="video/solo.mp4" type="video/mp4">
+        <source src="video/solo.ogv" type="video/ogg">
+        <source src="video/solo.webm" type="video/webm">
+    </video>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <!-- Overlay -->
+    <div id="home-overlay"></div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Home Content -->
+    <div id="home-content">
 
-            .full-height {
-                height: 100vh;
-            }
+        <div id="home-content-inner" class="text-center">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+            <div id="home-heading">
+                <h1 id="home-heading-1">Digital</h1><br>
+                <h1 id="home-heading-2">Creative <span>Agency</span></h1>
             </div>
+
+            <div id="home-text">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam in nihil minima unde qui nihil minima.</p>
+            </div>
+
+            <div id="home-btn">
+                <a class="btn btn-general btn-home smooth-scroll" href="#about" title="Start Now" role="button">Start Now</a>
+            </div>
+
         </div>
-    </body>
-</html>
+
+    </div>
+
+    <!-- Arrow Down -->
+    <a href="#about" id="arrow-down" class="smooth-scroll">
+        <i class="fa fa-angle-down"></i>
+    </a>
+
+</section>
+<!-- Home ends-->
+
+<!-- Contact -->
+<section id="contact">
+
+    <div class="content-box-md">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-6">
+
+                    <!-- Contact Left -->
+                    <div id="contact-left">
+
+                        <div class="vertical-heading">
+                            <h5>Who We Are</h5>
+                            <h2>Get<br>In <strong>Touch</strong></h2>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias modi est itaque aliquam sit, minima esse nihil mollitia no.</p>
+
+                        <div id="offices">
+
+                            <div class="row">
+
+                                <div class="col-md-6">
+
+                                    <div class="office">
+                                        <h4>United States</h4>
+                                        <ul class="office-details">
+                                            <li><i class="fa fa-mobile"></i><span>+(55) 879 58 87 46</span></li>
+                                            <li><i class="fa fa-envelope"></i><span>support@solo.com</span></li>
+                                            <li><i class="fa fa-map-marker"></i><span>524 Mina Street Building 05<br>Newyork, USA.</span></li>
+                                        </ul>
+                                    </div>
+
+                                </div>
+                                <div class="col-md-6">
+
+                                    <div class="office">
+                                        <h4>Australia</h4>
+                                        <ul class="office-details">
+                                            <li><i class="fa fa-mobile"></i><span>+(88) 457 87 74 87</span></li>
+                                            <li><i class="fa fa-envelope"></i><span>support@solo.com</span></li>
+                                            <li><i class="fa fa-map-marker"></i><span>507 Din Street Building 55 <br>Sydney, Australia.</span></li>
+                                        </ul>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                        <ul class="social-list">
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                        </ul>
+
+                    </div>
+
+                </div>
+                <div class="col-md-6">
+
+                    <!-- Contact Right -->
+                    <div id="contact-right">
+
+                        <form>
+                            <h4>Send Message</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa repell.</p>
+
+                            <div class="row">
+
+                                <div class="col-md-6 col-sm-6">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" id="name" placeholder="Your Name">
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-sm-6">
+                                    <div class="form-group">
+                                        <input type="email" class="form-control" id="email" placeholder="Email Address">
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-sm-6">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" id="phone" placeholder="Phone No.">
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-sm-6">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" id="subject" placeholder="Subject">
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="form-group">
+                                <textarea class="form-control" id="message" placeholder="Message"></textarea>
+                            </div>
+
+                            <div id="submit-btn">
+                                <a class="btn btn-general btn-yellow" href="#" title="Submit" role="button">Submit</a>
+                            </div>
+
+                        </form>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+<!-- Contact Ends -->
+
+<!-- Google Map -->
+<section id="google-map">
+    <div class="container-fluid">
+        <div class="row">
+            <div id="map"></div>
+        </div>
+    </div>
+</section>
+<!-- Google Map Ends -->
+
+@stop
