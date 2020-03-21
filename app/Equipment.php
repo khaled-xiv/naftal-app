077c0ba0a10a821040923fdd011accbcd45c8bc4
khaled-xiv <a.hachemi@esi-sba.dz>
2020-03-21 17:42:48 +0100

    updating jquery

--- app/Equipment.php
+++ app/Equipment.php
@@ -4,12 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use \Askedio\SoftCascade\Traits\SoftCascadeTrait;
 
 class Equipment extends Model
 {
     use SoftDeletes;
-    use SoftCascadeTrait;
 
     protected $dates = ['deleted_at'];
 
